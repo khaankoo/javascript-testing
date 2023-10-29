fa79ca7e4ddd9c0ebbf6b5115a392ae3aba94c9e
Khangerel Be-ochir <khangeerelbeochir@gmail.com>
2023-10-30 00:13:15 +0800

    deplay hiiv

---
+++ tests.js
@@ -0,0 +1,26 @@
+
+// const numbers = [1, 4, 3, 87, 6, 54, 34, 76, 1,];
+// for (let i = 1; i <= 10; i++){
+//     for(let j = 1; j < numbers.length; j++){
+//         console.log(i, numbers[j], i);
+//     };
+// };
+
+// let container = document.querySelector('.container');
+// let input1 = document.querySelector('.input1'); 
+// let input2 = document.querySelector('input2');
+// let result = document.querySelector('.result');
+// let button = document.querySelector('button');
+
+// function plus (){
+//     return input1.value + input2.value = result.value;
+//     button.onclick = result.value
+// }
+// plus();
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 0; i <=7; i++){
+    for(let j = 0; j < numbers.length; j++){
+        console.log(numbers[j], i);
+    };
+};
